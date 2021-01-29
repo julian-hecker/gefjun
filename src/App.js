79f2e833edcec37e427f9369b09e27fb36972cfd
Julian Hecker <julianheckerdev@gmail.com>
2021-01-29 15:30:38 -0500

    create moar components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
 import Menu from './pages/Menu';
+import Cart from './pages/Cart';
+import Account from './pages/Account';
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
                 <Navbar />
                 <Switch>
                     <Route exact path='/' component={Menu} />
+                    <Route exact path='/cart' component={Cart} />
+                    <Route exact path='/' component={Account} />
                 </Switch>
             </Router>
         </>
